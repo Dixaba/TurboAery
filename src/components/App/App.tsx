f94d20cf0556db077ef17dba80a60f2d7a9e604c
arsenykruglikov <kruglikov.arseny@gmail.com>
2019-05-25 21:59:52 +0300

    Config lcu-connector

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,67 +1,27 @@
-import React, { useState, useCallback, useMemo, SyntheticEvent, useEffect } from 'react';
-import Form from '../Form';
-import Input from '../Input';
-import Button from '../Button';
-const { ipcRenderer } = require('electron');
+import React, { useState, useEffect } from 'react';
+const LCUConnector = require('lcu-connector');
 
 const App: React.FunctionComponent = () => {
 
-  const [firstname, setFirstname] = useState('');
-  const [secondname, setSecondname] = useState('');
+  const [LCUData, setLCUData] = useState(null);
 
   useEffect(() => {
+    const connector = new LCUConnector();
 
-    console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+    connector.on('connect', (data: any) => {
+      setLCUData(data)
+    });
 
-    ipcRenderer.on('asynchronous-reply', (event: Event, arg: any[]) => {
-      console.log(event, arg) // prints "pong"
-    })
-    ipcRenderer.send('asynchronous-message', 'ping')
-  }, [])
+    // Start listening for the LCU client
+    connector.start();
 
-  const handleInput = useCallback(({ target }) => {
-    const { value, name } = target;
-    if (name === 'firstname') {
-      setFirstname(value)
-    } else {
-      setSecondname(value)
-    }
   }, []);
-
-  const handleSubmit = useCallback((event: SyntheticEvent) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    console.log();
-  }, []);
-
-  const memoFrom = useMemo(() => (
-    <Form>
-      <Input
-        type='text'
-        name='firstname'
-        label='Firstname'
-        placeholder='John'
-        onChange={handleInput}
-      />
-      <br />
-      <Input
-        type='text'
-        name='secondname'
-        label='Secondname'
-        placeholder='Doe'
-        onChange={handleInput}
-      />
-      <br />
-      <Button type={'submit'} onSubmit={handleSubmit}>Submit</Button>
-    </Form>), [handleInput, handleSubmit])
 
   return (
     <>
-      {memoFrom}
-      <br />
-      {firstname}
-      {secondname}
+      <pre>
+        {LCUData ? JSON.stringify(LCUData) : 'Waiting for connection'}
+      </pre>
     </>
   );
 }
